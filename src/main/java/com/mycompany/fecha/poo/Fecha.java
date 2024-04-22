23f9cdf149337e0b4d831a3a5afcc1ab912f563f
a <andress.gilg@konradlorenz.edu.co>
2024-04-22 16:14:28 -0500

    Se realizo  codigo de fecha

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,54 @@
 package com.mycompany.fecha.poo;
+import java.util.HashMap;
 
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+    private int dia;
+    private int mes;
+    private int ano;
+    public Fecha(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    public String fechaCorta() {
+        return dia + "/" + mes + "/" + ano;
+    }
+    public boolean validarFecha() {
+        if (ano <= 0 || mes < 1 || mes > 12) {
+            return false;
+        }
+        if (dia < 1 || dia > obtenerDiasEnMes()) {
+            return false;
+        }
+        return true;
+    }
+    private int obtenerDiasEnMes() {
+        int[] diasPorMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (mes == 2 && esBisiesto()) {
+            return 29;
+        }
+        return diasPorMes[mes - 1];
+    }
+    private boolean esBisiesto() {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+    public String mesLetra() {
+        HashMap<Integer, String> nombresMeses = new HashMap<>();
+        nombresMeses.put(1, "enero");
+        nombresMeses.put(2, "febrero");
+        nombresMeses.put(3, "marzo");
+        nombresMeses.put(4, "abril");
+        nombresMeses.put(5, "mayo");
+        nombresMeses.put(6, "junio");
+        nombresMeses.put(7, "julio");
+        nombresMeses.put(8, "agosto");
+        nombresMeses.put(9, "septiembre");
+        nombresMeses.put(10, "octubre");
+        nombresMeses.put(11, "noviembre");
+        nombresMeses.put(12, "diciembre");
+        return nombresMeses.getOrDefault(mes, "mes inválido");
+    }
+public String fechaLarga() {
+        return dia + " de " + mesLetra() + " de " + ano;
+    }
 }
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,20 @@
 package com.mycompany.fecha.poo;
-
+import java.util.Scanner;
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el día: ");
+        int dia = scanner.nextInt();
+        System.out.print("Ingrese el mes 1-12: ");
+        int mes = scanner.nextInt();
+        System.out.print("Ingrese el año: ");
+        int ano = scanner.nextInt();
+        Fecha fecha = new Fecha(dia, mes, ano);
+        if (fecha.validarFecha()) {
+            System.out.println("La fecha es válida.");
+            System.out.println("Fecha en formato largo: " + fecha.fechaLarga());
+        } else {
+            System.out.println("La fecha no es válida.");
+        }
+    }
 }
